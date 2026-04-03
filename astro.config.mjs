248217f691be4518ac1a83e40d148c6d34c288de
Jason E Perkins <dev@abovethefold.biz>
2026-04-03 12:42:58 -0400

    feat: configure Cloudflare adapter, Tailwind CSS, and Alpine.js integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs(), sitemap()],
+  adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
